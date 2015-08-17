932634395cd070daef1e0dddd14650ef7d0fe609
Rustem <grustik94@gmail.com>
2015-08-17 17:11:54 +0300

    up

---
+++ AppForCalc/Controllers/ComputeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using System.Web.Mvc;
+
+namespace AppForCalc.Controllers
+{
+    public class ComputeController : Controller
+    {
+        //
+        // GET: /Compute/
+
+        public ActionResult Index(string x)
+        {
+            DataTable dt = new DataTable();
+            var v = dt.Compute(x.Replace(" ", "+"), "");
+            ViewBag.Message = v;
+            return View();
+        }
+
+    }
+}

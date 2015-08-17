47cb6e81a16a7b93542b37efb4bc2d5d5b4fdb5e
Rustem <grustik94@gmail.com>
2015-08-17 18:12:57 +0300

    check

--- AppForCalc/Controllers/ComputeController.cs
+++ AppForCalc/Controllers/ComputeController.cs
@@ -12,7 +12,7 @@ namespace AppForCalc.Controllers
         {
             DataTable dt = new DataTable();
             var v = dt.Compute(x.Replace(" ", "+"), "");
-            ViewBag.Message = v;
+            ViewBag.Message = v+" успех (:";
             return View();
         }
 
